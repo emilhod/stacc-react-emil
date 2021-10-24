080603778896c8424ad2e21b39b3e2b315acab5b
emilhod <emilerkul123@gmail.com>
2021-10-24 14:09:10 +0200

    The data is now displayed in the console. Next step: Display on page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   const [kyc, setKyc] = useState(null);
 
   const getKycData = async () => {
-    const response = await axios.get("https://stacc-code-challenge-2021.azurewebsites.net/api/pep?name=Knut%20Arild%20Hareide");
-    setKyc(response.data);
+    const response = await axios.get("https://stacc-code-challenge-2021.azurewebsites.net/api/pep?name=Knut%20Arild%20Hareide").then((response) => console.log(response.data));
+    setKyc(response);
   };
   
   useEffect(() => {
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <div>
-      <p>{kyc}</p>
+      <p>Test</p>
  
     </div>
   )
